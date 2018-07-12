8599dcee4cfa10bd38477e7670b5abfe0691b18a
Sean Stills <sean.stills@gmail.com>
2018-07-11 20:48:11 -0400

    new version

---
+++ Js/index.js
@@ -0,0 +1,42 @@
+//this will alert the user when the logo is clicked that the homepage will be created soon
+const myMessage = ('Out Adventuring! Home Webpage Coming Soon');
+const myMessageDays = ('Out Adventuring! This Journey Will Be Added Soon');
+
+function myFunction(myMessage) {
+    alert(myMessage);
+}
+function myFunction(myMessageDays){
+    alert(myMessageDays);
+}
+
+
+//this starts the Code for the 2nd Day Journey
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
